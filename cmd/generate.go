1da14a19796f1c7ccd49ceafaaf7f60d5614c2d4
sarveshdakhore <sarveshd444@gmail.com>
2024-09-30 18:58:53 +0530

    Fixed issue Exit program after user presses ctrl + C #7

--- cmd/generate.go
+++ cmd/generate.go
@@ -3,20 +3,27 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/spf13/cobra"
+	"github.com/TejasGhatte/go-sail/internal/errors"
 	"github.com/TejasGhatte/go-sail/internal/scripts"
+	"github.com/spf13/cobra"
 )
 
 var CreateProjectCommand *cobra.Command
+
 func init() {
 	CreateProjectCommand = &cobra.Command{
-		Use: "create [project-name]",
+		Use:   "create [project-name]",
 		Short: "Creates a new go project",
-		Args: cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			project_name := args[0]
-			if err := scripts.CreateProject(project_name); err != nil {
-				fmt.Printf("Error creating project: %v\n", err)
+			projectName := args[0]
+			ctx := cmd.Context()
+			if err := scripts.CreateProject(ctx, projectName); err != nil {
+				if err == errors.ErrInterrupt {
+					fmt.Println("Program Exited: interrupt")
+				} else {
+					fmt.Printf("Program Exited: %v\n", err)
+				}
 			}
 		},
 	}
